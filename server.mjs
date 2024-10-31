1035998cec488dc100ada1ad59567623d01e122c
scarscarin <scarscarin@gmail.com>
2024-10-31 18:33:18 +0100

    separated archive.html

--- server.mjs
+++ server.mjs
@@ -198,5 +198,5 @@ app.get('/archive', async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
